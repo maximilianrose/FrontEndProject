68084abf8ccfce184e708a154dbb06746afb4fa1
Max Rose <Max Rose>
2017-03-06 19:48:34 +0000

    improve menu bar, some layout changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
 
+function randomreview()
+
+{
+
+  var mylinks= Math.round(Math.random()*9)
+var reviewlinks = new Array()
+reviewlinks[0] = "DragonsLair.html"
+reviewlinks[1] = "index.html"
+reviewlinks[2] = "battlefield1.html"
+reviewlinks[3] = "dishonored2.html"
+
+window.location= reviewlinks[mylinks]
+
+
+};
+
+
+
+
+
+
+
+
+
 //script for generating mario sprite
 
 var cx = document.querySelector('canvas').getContext('2d'),
